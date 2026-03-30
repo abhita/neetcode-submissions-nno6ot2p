0c27d0584536bbe96e9ddcb03036f9209eebd831
Abhita Lakkabathini <59249463+abhita@users.noreply.github.com>
2026-03-31 01:17:40 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        vector<int> stackElements;
+        vector<int> answer;
+        int i = temperatures.size()-1;
+        while( i>= 0 ){
+            //cout<<"i "<<i<<" "<< stackElements[i]<<endl;
+            if(stackElements.size() == 0){
+                answer.push_back(0);
+                stackElements.push_back(i);
+                i--;
+                continue;
+            }
+            if(temperatures[i] < temperatures[stackElements.back()]){
+                answer.push_back(stackElements.back() - i);
+                stackElements.push_back(i);
+                i--;
+            } else {
+                stackElements.pop_back();
+            }
+            
+        }
+        reverse(answer.begin(),answer.end());
+        return answer;
+    }
+};
+
+
+
+
+
+
+
