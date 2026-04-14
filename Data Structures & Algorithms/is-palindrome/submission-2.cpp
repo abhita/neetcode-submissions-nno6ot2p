01bb355402c98379ec97264ac8412366af5d0867
Abhita Lakkabathini <59249463+abhita@users.noreply.github.com>
2026-04-15 03:52:42 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> chards;
+
+        int size = s.size();
+        int start = 0;
+
+         int end = size-1;
+
+        while(end > start){
+            while(!alphaNum(s[start]) && start < end) start++ ;
+            while(!alphaNum(s[end]) && end > start) end--;
+            if(tolower(s[start]) != tolower(s[end])) return false;
+            cout<<s[start]<<s[end]<<endl;
+            start++;
+            end--;
+            
+        }
+        return true;
+    }
+
+    bool alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
+};
